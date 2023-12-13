bff959591d0fce10d8232a20e4971cb83050428d
Дмитрий Фалей <dmitryfaley@MacBook-Pro-Dmitrij.local>
2023-12-13 20:27:13 +0300

    Верстка

--- app/content/themes/gulp-theme/inc/sidebar-form.php
+++ app/content/themes/gulp-theme/inc/sidebar-form.php
@@ -1,24 +1,39 @@
-<section class="connect <?php if ( is_page( '60' ) ) {echo 'team-from';} ?> ">
+<div class="rating">
   <div class="container">
-    <div class="connect__content">
-      <div class="connect__form">
-        <p class="title">Связаться с нами</p>
-        <!-- <form method="post" class="form">
-          <input type="text" name="name" placeholder="Имя" required minlength="2" />
-          <input type="tel" name="phone" placeholder="Телефон" required />
-          <div class="form__button">
-            <button class="button">Отправить</button>
-            <p>Нажимая на кнопку "Отправить", вы соглашаетесь с <a href="#">условиями политики конфиденциальности</a>
-            </p>
-          </div>
-        </form> -->
-        <?php echo do_shortcode('[contact-form-7 id="18" title="Связаться с нами"]'); ?>
+    <div class="rating__text">
+      <p class="balls">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="26" viewBox="0 0 28 26" fill="none">
+          <path
+            d="M13.0489 0.927047C13.3483 0.00573683 14.6517 0.00573993 14.9511 0.927051L17.3677 8.36475C17.5016 8.77677 17.8855 9.05573 18.3188 9.05573H26.1392C27.1079 9.05573 27.5107 10.2953 26.727 10.8647L20.4001 15.4615C20.0496 15.7161 19.903 16.1675 20.0369 16.5795L22.4535 24.0172C22.7529 24.9385 21.6984 25.7047 20.9147 25.1353L14.5878 20.5385C14.2373 20.2839 13.7627 20.2839 13.4122 20.5385L7.08533 25.1353C6.30162 25.7047 5.24714 24.9385 5.54649 24.0172L7.96315 16.5795C8.09702 16.1675 7.95036 15.7161 7.59987 15.4615L1.27299 10.8647C0.489277 10.2953 0.892056 9.05573 1.86078 9.05573H9.68123C10.1145 9.05573 10.4984 8.77677 10.6323 8.36475L13.0489 0.927047Z"
+            fill="white" />
+        </svg>
+        9.1 баллов</p>
+      <p class="rew">Рейтинг коммуникационной компании GBS на основании <span>58 отзывов</span> заказчиков</p>
+    </div>
+  </div>
+</div>
+
+<section class="connect">
+  <div class="container">
+    <div class="connect__form">
+      <div class="form__text">
+        <p class="title">Вы можете рассказать нам о своем проекте и о том, как мы можем помочь</p>
+        <p class="sibtitle">
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+        <div class="btn__dowlonds">
+          <a href="#">скачать бриф</a>
+          <a href="#">скачать портфолио</a>
+        </div>
       </div>
-      <div class="connect__icons">
-        <img class="icon" src="<?php echo get_template_directory_uri() ?>/assets/img/icons/ee.svg" alt="icon">
-        <img class="icon" src="<?php echo get_template_directory_uri() ?>/assets/img/icons/about2.svg" alt="icon">
-        <img class="icon" src="<?php echo get_template_directory_uri() ?>/assets/img/icons/about4.svg" alt="icon">
-      </div>
+      <form method="post" class="form">
+        <label for="name">Ваше имя</label>
+        <input type="text" name="name" placeholder="Имя" required minlength="2" />
+        <label for="phone">Номер телефона</label>
+        <input type="tel" name="phone" placeholder="+375 (--) --- -- --" required />
+        <button class="button">Отправить</button>
+      </form>
+      <?php //echo do_shortcode('[contact-form-7 id="18" title="Связаться с нами"]'); ?>
     </div>
   </div>
 </section>
